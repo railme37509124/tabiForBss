cd1ac7afa3921995505b691b409853e2e533860a
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-11-01 19:28:12 +0000

    Create main.test.lua

---
+++ potetisastupidgay/main.test.lua
@@ -0,0 +1,6 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/railme37509124/tabiForBss/refs/heads/main/potetisastupidgay/uilibrary.lua", true))()
+
+Test = library:New("Tabi V1.0.0 🎃")
+Tab = Test:Tab("Test")
+Folder = Tab:Folder("Tabi")
+Folder:Toggle("Altered is cute")
